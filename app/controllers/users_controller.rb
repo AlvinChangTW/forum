d9c1d54c11138e45fd95349618cad467b2948029
birdbrother <birdbrother@gmail.com>
2016-10-10 19:08:38 +0800

    pager to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def profile
     @user = User.includes(:topics,:comments).find(params[:id])
-
+    @comments = @user.comments.page(params[:page]).per(5)
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
   validates_presence_of :name
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   has_many :topic_categoryships
   has_many :categories, :through => :topic_categoryships
   belongs_to :user
