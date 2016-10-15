3ba8bff8fb0cf0a5776034cf3042a3ff9d22bbed
birdbrother <birdbrother@gmail.com>
2016-10-15 21:19:20 +0800

    finish like change with ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,19 @@
 class LikesController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
-    @like = Like.new
-    @like.topic_id = @topic.id
-    @like.user_id = current_user.id
-    @like.save
-    render "topics/show"
+    # @like = Like.new
+    # @like.topic_id = @topic.id
+    # @like.user_id = current_user.id
+    # @like.save
+    #全部變下面的一行
+    #Like.create( :topic_id => @topic.id, :user_id => current_user.id )
+    #或是下面這行也行
+    current_user.liked_topics << @topic
+    @like = current_user.likes.find_by_topic_id(@topic)
+    respond_to do |format|
+      format.js
+    end
+    #render "topics/show"
   end
   def destroy
     @topic = Topic.find(params[:topic_id])
@@ -13,6 +21,13 @@ class LikesController < ApplicationController
     @like.destroy
     @like = nil
     #因為@like.destroy後資料庫把內容刪除後沒有把@like撈出來暫存的資料刪除
-    render "topics/show"
+    respond_to do |format|
+      format.js
+    end
+    #render "topics/show"
   end
+  private
+  # def like_params
+  #   params.require(:like).permit(:topic_id, :user_id)
+  # end
 end
