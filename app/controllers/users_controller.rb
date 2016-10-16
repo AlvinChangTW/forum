06097bc0bd8ddf1e794a73a56f8aac9fd376df36
birdbrother <birdbrother@gmail.com>
2016-10-16 10:35:02 +0800

    add likes collection list to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def profile
-    @user = User.includes(:topics,:comments).find(params[:id])
+    @user = User.includes(:topics,:comments,:likes).find(params[:id])
     @comments = @user.comments.page(params[:page]).per(5)
+    @likes = @user.liked_topics
+
   end
 end
