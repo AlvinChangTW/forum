9910235449c36fcd56039a1d1b602c58ebdb7dfd
birdbrother <birdbrother@gmail.com>
2016-10-21 20:41:16 +0800

    complete mysql

--- db/schema.rb
+++ db/schema.rb
@@ -12,76 +12,74 @@
 
 ActiveRecord::Schema.define(version: 20161021034525) do
 
-  create_table "categories", force: :cascade do |t|
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_categories_on_name"
+    t.index ["name"], name: "index_categories_on_name", using: :btree
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "content"
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.text     "content",    limit: 65535
+    t.integer  "topic_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "user_id"
+    t.index ["topic_id"], name: "index_comments_on_topic_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.integer  "user_id"
     t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
-    t.string   "status"
-    t.index ["topic_id"], name: "index_comments_on_topic_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["topic_id"], name: "index_likes_on_topic_id", using: :btree
+    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["topic_id"], name: "index_likes_on_topic_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "topic_categoryships", force: :cascade do |t|
+  create_table "topic_categoryships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer  "topic_id"
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_topic_categoryships_on_category_id"
-    t.index ["topic_id"], name: "index_topic_categoryships_on_topic_id"
+    t.index ["category_id"], name: "index_topic_categoryships_on_category_id", using: :btree
+    t.index ["topic_id"], name: "index_topic_categoryships_on_topic_id", using: :btree
   end
 
-  create_table "topics", force: :cascade do |t|
+  create_table "topics", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "name"
-    t.text     "content"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.integer  "comments_count", default: 0
+    t.text     "content",        limit: 65535
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.integer  "comments_count",               default: 0
     t.integer  "user_id"
-    t.integer  "view",           default: 0
-    t.string   "status"
-    t.index ["user_id"], name: "index_topics_on_user_id"
-    t.index ["view"], name: "index_topics_on_view"
+    t.integer  "view",                         default: 0
+    t.index ["user_id"], name: "index_topics_on_user_id", using: :btree
+    t.index ["view"], name: "index_topics_on_view", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                        default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.string   "nickname"
-    t.text     "description"
+    t.text     "description",            limit: 65535
     t.string   "role"
     t.string   "fb_uid"
     t.string   "fb_token"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["fb_uid"], name: "index_users_on_fb_uid"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["fb_uid"], name: "index_users_on_fb_uid", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
